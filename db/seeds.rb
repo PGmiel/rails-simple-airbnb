f96affc0a7c30b208c7578b2e0ae63939790ccb3
Pierre-Guillaume <80978648+PGmiel@users.noreply.github.com>
2021-05-26 22:52:28 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  flat = Flat.new(
+  name:     Faker::Games::WorldOfWarcraft.hero,
+  address:  Faker::Address.city,
+  description: Faker::Quote.famous_last_words,
+  price_per_night: rand(70..100),
+  number_of_guests: rand(2..6)
+  )
+  flat.save!
+end
